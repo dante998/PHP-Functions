b4641c079e2fbf884de151400aa3e5ce7fca56b9
Mihael Kamenov <Mihael@example.com>
2023-04-05 11:32:46 +0300

    del

--- Callback functions/fruits.php
+++
@@ -1,9 +0,0 @@
-<?php
-function my_callback($item): int {
-  return strlen($item);
-}
-
-$strings = ["apple", "orange", "banana", "coconut"];
-$lengths = array_map("my_callback", $strings);
-
-print_r($lengths);
